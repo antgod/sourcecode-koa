9c34eb94ea500a54685f5b9e63b40ee53d1c000b
lihongji <lihongji@meituan.com>
2016-04-23 15:24:41 +0800

    add

---
+++ koa分析/test.js
@@ -0,0 +1,14 @@
+var a=function * (){
+};
+var b=function  (){
+};
+
+var c={};
+
+var d="abc";
+
+
+console.log(a.constructor.name);
+console.log(b.constructor.name);
+console.log(c.constructor.name);
+console.log(d.constructor.name);
